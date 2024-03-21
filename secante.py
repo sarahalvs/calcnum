ee153e8f9a79fe431c10740cfae784175123e77a
Sarah Alves <160775415+sarahalvs@users.noreply.github.com>
2024-03-21 20:14:34 -0300

    secante.py

--- secante.py
+++ secante.py
@@ -4,28 +4,31 @@ import math
 def f(x):
     return x / math.exp(x) - 0.2 * x**2 + 17
 
-# Método secante com o quociente
-def secante_quociente(f, x0, x1, epsilon, max_iter):
-    for i in range(max_iter):
+# Método secante com o quociente de definição
+def secante(f, x0, x1, epsilon, max_iter):
+    iteracoes = 0
+    while iteracoes < max_iter:
         f_x0 = f(x0)
         f_x1 = f(x1)
         x2 = (x0 * f_x1 - x1 * f_x0) / (f_x1 - f_x0)
         if abs(x2 - x1) < epsilon:
-            return x2
+            return x2, iteracoes
         x0 = x1
         x1 = x2
-    return None
+        iteracoes += 1
+    return None, iteracoes
 
 # Intervalos, precisão e iteração
 intervalos = [(-3, -2), (9, 10)]
-epsilon = 1e-14
-max_iter = 1000
+epsilon = 1.0e-7
+max_iter = 5
 
 # Iterações para cada intervalo
 for intervalo in intervalos:
     print(f"Iterações para o intervalo {intervalo}:")
-    raiz_encontrada = secante_quociente(f, intervalo[0], intervalo[1], epsilon, max_iter)
-    if raiz_encontrada is not None:
-        print(f"Raiz encontrada: {raiz_encontrada}")
+    raiz, iteracoes = secante(f, intervalo[0], intervalo[1], epsilon, max_iter)
+    if raiz is not None:
+        print(f"Raiz encontrada: {raiz}")
+        print(f'Número de iterações: {iteracoes}')
     else:
         print("Não foi possível encontrar a raiz com a precisão especificada.")
